36253befcf13889e9387921193d1dfb17cdbc750
Adam Sokol <adambsokol@gmail.com>
2020-11-17 15:48:51 -0500

    changed terminology in matcher.py to be consistent with air.py

--- purple_haze/matcher.py
+++ purple_haze/matcher.py
@@ -3,8 +3,8 @@ To run do the following commands (as of 11/13):
 
 import air as air
 import matcher as match
-sensor_data = air.files_to_dataframe(glob.glob('../data/purple_air/*'))
-matched_ses_data = match.station_matcher(sensor_data)
+data_stream_info = air.files_to_dataframe(glob.glob('../data/purple_air/*'))
+matched_ses_data = match.station_matcher(data_stream_info)
 
 Note, you may have to change the imports depending on where you're running this from. If you are in the purple_haze folder this works.
 '''
@@ -16,9 +16,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-def get_sensor_names(data_array , name10):
-    '''This is a helper function that grabs the sensors in each census tract from the joined dataframe to append to the SES data
-    If there are no sensors in a census tract it returns a nan.
+def get_stream_names(data_array , name10):
+    '''This is a helper function that grabs the data stream file names in each census tract from the joined dataframe to append to the SES data
+    If there are no data streams in a census tract it returns a nan.
     '''
     try:
         return data_array['all_names'][name10]
@@ -26,10 +26,10 @@ def get_sensor_names(data_array , name10):
         return np.nan
 
 
-def station_matcher(lat_lons):
-    '''This function will take in a dataframe that has the sensor file name, its lat, its lon, and possibly other stuff.
+def station_matcher(data_stream_df):
+    '''This function will take in a dataframe that has the data stream file name, its lat, its lon, and possibly other stuff.
     It will then load in the SES data and match the purple air files to their respective census tract.
-    Ultimately, it will return the SES data with a new column that is a list of the filenames of all Purple air stations in that census
+    Ultimately, it will return the SES data with a new column that is a list of the filenames of all Purple air data streams in that census
     tract.
     
     '''
@@ -41,10 +41,10 @@ def station_matcher(lat_lons):
     
     #Assume that the sensors lat/lon are in a Pandas Dataframe with columns of Lat and Lon
     #We will then convert it to a GeoDataFrame
-    sensor_gdf = gpd.GeoDataFrame(lat_lons, geometry=gpd.points_from_xy(lat_lons['lon'], lat_lons['lat']), crs="EPSG:4326")
+    data_stream_gdf = gpd.GeoDataFrame(data_stream_df, geometry=gpd.points_from_xy(data_stream_df['lon'], data_stream_df['lat']), crs="EPSG:4326")
     
     #Combine them with INNER!!! This retains the sensor file names
-    combined = sjoin(new_ses_data, sensor_gdf, how='inner', op='intersects')
+    combined = sjoin(new_ses_data, data_stream_gdf, how='inner', op='intersects')
     
     #Now we need to combine rows that have multiple entries
     #This 'NAME10' should be one unique name for each census tract. There are others too.
@@ -58,7 +58,7 @@ def station_matcher(lat_lons):
     #This is adding a new column to the original ses data that is called sensor_names
     #It has the same data that is in aggregate, but we have to do it this funny way I think (?)
     #Basically it goes row by row and uses our helper function (get_sensor_names) to grab the sensor names from aggregate
-    new_ses_data['sensor_file_names'] = new_ses_data.apply(lambda row: get_sensor_names(aggregate , row['NAME10']), axis=1)
+    new_ses_data['data_stream_file_names'] = new_ses_data.apply(lambda row: get_sensor_names(aggregate , row['NAME10']), axis=1)
 
     return new_ses_data
     
