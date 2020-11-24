c872fcc7a2614a530f3b088841ac6750f1679631
Greta Shum <gretashum@gmail.com>
2020-11-24 12:24:24 -0800

    added tests, .travis.yml

---
+++ tests/test_matcher.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# to run me, type in terminal: 'python -m unittest tests.test_matcher'
+'''Testing for errors in matcher.py'''
+import unittest
+from purple_haze import matcher
+import numpy as np
+
+class MatcherTest(unittest.TestCase):
+    '''tests matcher.py for accuracy and errors'''
+    # smoke test
+    def test_smoke(self):
+        '''first, a smoke test to make sure output of matcher.py is not zero'''
+	test = 'test'
+        assert matcher.files_string(test) is not None
+
+    # one-shot test (add next)
+
+if __name__ == '__main__':
+    unittest.main()
