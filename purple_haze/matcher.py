a0a3b5921e2e9d1c60eced8ab22c3bcd67653f2e
Tyler Cox <tylersc@uw.edu>
2020-12-02 07:33:37 -0800

    Made matcher.py pep8 compliant

--- purple_haze/matcher.py
+++ purple_haze/matcher.py
@@ -2,7 +2,7 @@
 
 Functions:
 
-station_matcher(data_stream_df): joins Purple Air CSV file dataframe 
+station_matcher(data_stream_df): joins Purple Air CSV file dataframe
 with census tract socioeconomic data
 
 get_stream_names(data_array, name10): helper functions that gets the
@@ -22,9 +22,9 @@ matched_ses_data = match.station_matcher(data_stream_info)
 
 import geopandas as gpd
 from geopandas.tools import sjoin
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+
 
 def station_matcher(data_stream_df):
     """ Matches Purple Air data with census tracts
@@ -42,62 +42,53 @@ def station_matcher(data_stream_df):
 
     new_ses_data (pandas dataframe): tract-level socioeconomic dataset
     with an added column containing the paths to the CSV files for
-    DataStreams contained within each tract. The CSV file paths are 
+    DataStreams contained within each tract. The CSV file paths are
     provided as a single string, with paths separated by commas.
     """
-    
     # load socioeconomic dataset
     ses_file = '../data/seattle_ses_data/ses_data.shp'
     ses_data = gpd.read_file(ses_file)
     new_ses_data = ses_data.to_crs(epsg=4326)
-    
-    # convert input to GeoDataFrame using lat/lon 
+    # convert input to GeoDataFrame using lat/lon
     data_stream_gdf = gpd.GeoDataFrame(
-        data_stream_df, 
+        data_stream_df,
         geometry=gpd.points_from_xy(
             data_stream_df['lon'],
             data_stream_df['lat']),
         crs="EPSG:4326")
-    
     # join the two dataframes (must use "inner" to rerain sensor file names)
-    combined = sjoin(new_ses_data, 
+    combined = sjoin(new_ses_data,
                      data_stream_gdf,
                      how='inner',
                      op='intersects')
-    
     # combine rows from same tract ('NAME10' is census tract name)
     grouped = combined.groupby('NAME10')
-    
     # make new column containing CSV file names separated by commas
     aggregate = grouped.agg(
-        all_names=pd.NamedAgg(column='file',aggfunc=','.join))
-    
+        all_names=pd.NamedAgg(column='file', aggfunc=','.join))
     # add CSV file names to SES dataset
     new_ses_data['data_stream_file_names'] = new_ses_data.apply(
-        lambda row: get_stream_names(aggregate , row['NAME10']), axis=1)
-    
+        lambda row: get_stream_names(aggregate, row['NAME10']), axis=1)
     # number of datastream CSV files for each tract
     new_ses_data['datastream_counts'] = new_ses_data.apply(
         lambda row: count_csv_files(row['data_stream_file_names']),
         axis=1)
-
     # number of Sensors in each tract (number of CSV files divided by 4)
     new_ses_data['sensor_counts'] = new_ses_data['datastream_counts'] / 4
-
     return new_ses_data
 
 
 def get_stream_names(data_array, name10):
     """Retrieves data stream files for census tract
-    
+
     Helper function that grabs the data stream file names in each
     census tract from the joined dataframe to append to the SES data.
     If there are no data streams in a census tract it returns NaN.
-    
+
     Args:
         data_array (Pandas or GeoPandas dataframe): the combined array
-        containing identifiers of the census tract (NAME10) and a column of comma
-        separated values of each datastream in that census tract.
+        containing identifiers of the census tract (NAME10) and a column
+        of comma separated values of each datastream in that census tract.
         name10 (string): a unique string that identifies each census tract
 
     Returns:
@@ -106,15 +97,14 @@ def get_stream_names(data_array, name10):
     """
     try:
         names = data_array['all_names'][name10]
-
-    except:
+    except NameError:
         names = np.nan
-    
     return names
-    
+
+
 def count_csv_files(files_string):
     """Count number of DataStream CSV files in census tract
-    
+
     Parses a string of CSV file paths to determine how many
     individual files there are.
 
@@ -125,17 +115,7 @@ def count_csv_files(files_string):
     Returns:
         counts (int): number of CSV files
     """
-    
     new_files_string = str(files_string)
-    counts = 0
-
     if new_files_string == 'nan':
         return 0
-
-    else:
-        counts = new_files_string.count(",") + 1
-        return counts
-
-
-
-    
+    return new_files_string.count(",") + 1
