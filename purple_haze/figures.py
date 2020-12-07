19daf9f4c1e48986e203c57eb950cbf81924dd7d
Adam Sokol <adambsokol@gmail.com>
2020-12-07 15:48:28 -0500

    made figures.py pep8-compliant

--- purple_haze/figures.py
+++ purple_haze/figures.py
@@ -2,21 +2,22 @@
 
 Functions:
 
-sensor_count_plotting(ses_metric, renamed_ses_data): creates the scatter
-plot plotting a mean SES metric vs the number of sensors in a census tract along
-with a linear regression and r^2 value.
+sensor_count_plotting(ses_metric, ses_data): makes a
+scatterplot of census tract socioeconomic metrics against the number of
+Purple Air sensors in each tract.
 
-aqi_plotting(ses_metric, aqi_metric,renamed_ses_data): creates the scatter
-plot plotting an ses_metric and aqi_metric for each census tract along with a linear
-regression and r^2 value.
+aqi_plotting(ses_metric, aqi_metric, ses_data): makes a
+scatterplot of a census tract socioeconomic and air quality metrics.
 
-make_altair_chart(renamed_ses_data,SES_metric): creates an Altair chart
-mapping the census tract data and coloring the tracts by an SES metric in a dropdown.
+make_altair_chart(ses_data, SES_metric): creates an Altair
+chart mapping the census tract data and coloring the tracts by a
+selected socioeconomic metric.
 
-make_widgets(renamed_ses_data): creates two interactive widgets for both scatter plots.
+make_widgets(ses_data): creates two interactive widgets for
+both scatter plots.
 
-display_app(renamed_ses_data): displays the application and creates the layout for
-all three interactive plots, ready for use by Voila.
+display_app(ses_data): displays application and creates the
+layout for all three interactive plots, ready for use by Voila.
 
 Purple Haze Project Group
 CSE 583 Fall 2020
@@ -30,193 +31,231 @@ import scipy.stats as sc_stats
 import altair as alt
 import matcher as match
 
-def sensor_count_plotting(ses_metric, renamed_ses_data):
-    """Creates a scatter plot with linear regression of an SES metric and sensor counts
 
-    This function creates a scatter plot chart of an averaged SES metric value against
-    the number of PurpleAir sensors within a census tract. Additionally, a linear
-    regression is performed and plotted, with the r^2 value displayed on the figure.
+def sensor_count_plotting(ses_metric, ses_data):
+    """Scatter plot of SES metric and sensor counts
+
+    This function groups the census tracts by the number of Purple Air
+    sensors in each tract, then computes and plots the mean
+    socioeconomic metric among the tracts in each group. The generated
+    plot has the number of sensors on the x-axis and the socioeconomic
+    metric on the y-axis. A linear regression line is also plotted and
+    the correlation coefficient displayed.
 
     Args:
-        - ses_metric (string): name of the SES metric to display on the y-axis
-        - renamed_ses_data (GeoDataFrame): contains the census tract shapefile data
-          along with additional aqi computations. Some column names were renamed
+        - ses_metric (string): socioeconomic metric (x-coordinate)
+        - ses_data (GeoDataFrame): contains census tract shapefile data
+          along with additional Purple Air sensor information.
     """
-    # group by the number of sensors in the tract
-    grouped = renamed_ses_data.groupby('sensor_counts')
+    # Group by census tract sensor count.
+    grouped = ses_data.groupby("sensor_counts")
 
-    # number of sensors in tract (x coordinate)
+    # Number of sensors in tract (x-coordinate).
     num_sensors = np.array([name for name, group in grouped])
 
-    # metric of interest (y coordinate)
+    # Get mean of socioeconomic metric for each sensor count group.
     stat = np.array([group[ses_metric].mean() for name, group in grouped])
 
-    # number of census tracts in the group
-    tract_counts = np.array([group['OBJECTID'].count() for name, group in grouped])
+    # Number of census tracts in each sensor count group.
+    tract_counts = np.array(
+        [group["OBJECTID"].count() for name, group in grouped])
 
-    # regression
-    slope, intercept, r_value, p_value, std_err = sc_stats.linregress(num_sensors, stat)
+    # Linear regression.
+    m, b, corr_coef, p, std_err = sc_stats.linregress(num_sensors, stat)
 
-    # plot
-    fig, ax = plt.subplots(figsize=(9, 5.4))
-    ax.scatter(num_sensors, stat, s=tract_counts*10)
-    ax.plot(num_sensors, num_sensors * slope + intercept, color='k')
-    ax.text(x=0.7, y=0.9, s = 'r^2 = ' + str(round(r_value ** 2, 3)), fontsize=16,
-             color='r', transform=ax.transAxes)
-    plt.title(ses_metric + ' versus number of sensors by census tract', fontsize=18)
+    # Make the plot.
+    plt.figure(figsize=(9, 5.4))
+    plt.scatter(num_sensors, stat, s=tract_counts*10)
+    plt.plot(num_sensors, num_sensors * m + b, color="k")
+    plt.text(
+        x=0.7, y=0.9, s=r"r$^2$ = {:.3f}".format(round(corr_coef ** 2, 3)),
+        fontsize=16, color="r", transform=plt.gca().transAxes)
+    plt.title(
+        f"{ses_metric} vs. number of sensors in census tract",
+        fontsize=18)
     plt.ylabel(ses_metric, fontsize=16)
-    plt.xlabel('Sensors in census tract', fontsize=16)
+    plt.xlabel("Sensors in census tract", fontsize=16)
     plt.xticks(fontsize=14)
     plt.yticks(fontsize=14)
     plt.show()
 
-def aqi_plotting(ses_metric, aqi_metric, renamed_ses_data):
-    """Creates a scatter plot with linear regression of an SES metric and an AQI metric
+    return
 
-    This function creates a scatter plot chart of an SES metric value for one census
-    tract against an AQI metric. Additionally, a linear regression is performed and plotted,
-    with the r^2 value displayed on the figure.
-        Args:
-        - ses_metric (string): name of the SES metric to display on the y-axis
-        - aqi_metric (string): name of the AQI metric to display on the x-axis
-        - renamed_ses_data (GeoDataFrame): contains the census tract shapefile data
-          along with additional aqi computations. Some column names were renamed
+
+def aqi_plotting(ses_metric, aqi_metric, ses_data):
+    """Scatterplot of socioeconomic vs air quality metrics
+
+    This function creates a scatter plot chart of the selected air
+    quality (x-axis) and socioeconomic (y-axis) metrics. Each point on
+    the plot represents one census tract. A linear regression line is
+    also plotted and the correlation coefficient displayed.
+
+    Args:
+        - ses_metric (string): socioeconomic metric (y-axis)
+        - aqi_metric (string): air quality metric (x-axis)
+        - ses_data (GeoDataFrame): contains census tract shapefile data
+          along with additional Purple Air sensor information.
     """
 
-    # get AQI and SES data
-    aqi = renamed_ses_data[aqi_metric]
-    counts = renamed_ses_data[ses_metric]
+    # Get the selected air quality and SES data.
+    aqi = ses_data[aqi_metric]
+    counts = ses_data[ses_metric]
 
-    # regression (requires removal of NaN)
+    # Remove NaNs and compute regression.
     mask = ~np.isnan(aqi) & ~np.isnan(counts)
-    slope, intercept, r_value, p_value, std_err = sc_stats.linregress(aqi[mask], counts[mask])
+    m, b, corr_coef, p, std_err = sc_stats.linregress(aqi[mask], counts[mask])
 
-    # plot
-    fig, ax = plt.subplots(figsize=(9, 5.4))
-    ax.scatter(aqi, counts)
-    aqi_space = np.linspace(np.amin(aqi), np.amax(aqi))
-    ax.plot(aqi_space, aqi_space * slope + intercept, color='k')
-    ax.text(x=0.7, y=0.9, s = 'r^2 = ' + str(round(r_value ** 2, 3)), fontsize=16,
-             color='r', transform=ax.transAxes)
-    plt.title(ses_metric + ' versus ' + aqi_metric, fontsize=16)
+    # Make the figure.
+    plt.figure(figsize=(9, 5.4))
+    plt.scatter(aqi, counts)
+    aqi_pts = np.linspace(np.amin(aqi), np.amax(aqi))
+    plt.plot(aqi_pts, aqi_pts * m + b, color="k")
+    plt.text(
+        x=0.7, y=0.9, s=r"r$^2$ = {:.3f}".format(round(corr_coef ** 2, 3)),
+        fontsize=16, color="r", transform=plt.gca().transAxes)
+    plt.title(f"{ses_metric} vs. {aqi_metric}", fontsize=16)
     plt.ylabel(ses_metric, fontsize=16)
     plt.xlabel(aqi_metric, fontsize=16)
     plt.xticks(fontsize=14)
     plt.yticks(fontsize=14)
     plt.show()
 
-def make_altair_chart(renamed_ses_data, ses_metric):
-    """Creates a map of Seattle divided by census tract and colored by an SES metric
+    return
 
-    This function creates a map of Seattle by plotting shapefile data of each census tract
-    using Altair to easily create the map and to allow for interactions.
-        Args:
-        - renamed_ses_data (GeoDataFrame): contains the census tract shapefile data
-          along with additional aqi computations. Some column names were renamed
-        - SES_metric (string): name of the SES metric to color the map with
-        Returns:
+
+def make_altair_chart(ses_data, ses_metric):
+    """Creates a tract-level map of the selected tract-level metric.
+
+    This function creates a map of Seattle showing the selected
+    socioeconomic or air quality metric at the census tract level. The
+    map is created using Altair with interactions enabled.
+
+    Args:
+        - ses_data (GeoDataFrame): contains census tract shapefile data
+          along with additional Purple Air sensor information.
+        - ses_metric (string): socioeconomic metric to be plotted.
+    Returns:
         - chart (Altair chart object): the map as a chart object
     """
 
-    # Convert geopandas data to geojson for use with Altair
-    data  = alt.InlineData(values = renamed_ses_data.__geo_interface__,
-                           format = alt.DataFormat(property='features',type='json'))
+    # Convert geopandas data to geojson for use with Altair.
+    data = alt.InlineData(
+        values=ses_data.__geo_interface__,
+        format=alt.DataFormat(property="features", type="json"))
 
-    # Choose the renderer to properly display in Jupyter Notebook, Jupyter Lab, and Voila
-    alt.renderers.enable('kaggle')
+    # Choose renderer for proper display with Jupyter and Voila.
+    alt.renderers.enable("kaggle")
 
-    # Make the chart
+    # Make the chart.
     chart = alt.Chart(data).mark_geoshape(
-        stroke='black', strokeWidth=0.5
+        stroke="black",
+        strokeWidth=0.5
     ).encode(
-        color = alt.Color('properties.%s' % ses_metric,
-                          type='quantitative', title='%s' % ses_metric),
-        tooltip=[alt.Tooltip('properties.NAME10:N', title='Census Tract')]
+        color=alt.Color(
+            f"properties.{ses_metric}",
+            type="quantitative",
+            title=ses_metric),
+        tooltip=[alt.Tooltip("properties.NAME10:N", title="Census Tract")]
     ).properties(
-        width = 400,
-        height = 750,
+        width=400,
+        height=750,
     )
 
     return chart
 
-def make_widgets(renamed_ses_data):
-    """Creates the scatter plots as interactive figures using ipywidgets
 
-    This function creates the interactive widget instances for the scatter plots
-    created by the sensor_count_plotting and aqi_plotting functions. The list of
-    metrics for both SES and AQI data are found within this function.
+def make_widgets(ses_data):
+    """Creates interactive scatter plots using ipywidgets.
+
+    This function creates interactive widget instances for the scatter
+    plots created by sensor_count_plotting and aqi_plotting. The list
+    of socioeconomic and air quality metrics appearing in the dropdown
+    menus are established here.
+
     Args:
-        - renamed_ses_data (GeoDataFrame): contains the census tract shapefile data
-        along with additional aqi computations. Some column names were renamed
+        - ses_data (GeoDataFrame): contains census tract shapefile data
+          along with additional Purple Air sensor information.
     Returns:
-        - sensor (widget): An interactive dropdown widget changing the SES metric
-        - aqi (widget): An interactive dropdown widget changing the AQI metric
+        - sensor (widget): interactive dropdown widget for selecting
+          socioeconomic metrics.
+        - aqi (widget): interactive dropdown widget for selecting air
+          quality metrics.
     """
 
-    # metric lists
-    all_ses_metrics = match.ses_name_mappings.values()
-    ses_metric_list = [metric for metric in all_ses_metrics if 'quintile' not in metric]
+    # Retrieve list of socioeconomic matrics.
+    ses_metrics = match.ses_name_mappings.values()
 
+    # Remove quintile metrics since they are encoded as strings.
+    ses_metric_list = [met for met in ses_metrics if "quintile" not in met]
+
+    # List of air quality metric options.
     aqi_metric_list = [
-        'mean_aqi',
-        'exposure_aqi100',
-        'exposure_aqi150',
-        'mean_aqi_no_smoke',
-        'exposure_aqi100_no_smoke',
-        'exposure_aqi150_no_smoke'
+        "mean_aqi",
+        "exposure_aqi100",
+        "exposure_aqi150",
+        "mean_aqi_no_smoke",
+        "exposure_aqi100_no_smoke",
+        "exposure_aqi150_no_smoke"
     ]
 
-    # interactive widgets
-    sensor = widgets.interactive(sensor_count_plotting,
-                 ses_metric=widgets.Dropdown(
-                     options=ses_metric_list,
-                     description='Socioeconomic metric'),
-                 renamed_ses_data=widgets.fixed(renamed_ses_data))
+    # Build interactive widgets.
+    sensor_widget = widgets.interactive(
+        sensor_count_plotting,
+        ses_metric=widgets.Dropdown(
+            options=ses_metric_list,
+            description="Socioeconomic metric"),
+        ses_data=widgets.fixed(ses_data))
 
-    aqi = widgets.interactive(aqi_plotting,
-                 ses_metric=widgets.Dropdown(
-                     options=ses_metric_list,
-                     description='Socioeconomic metric'),
-                 aqi_metric = widgets.Dropdown(
-                     options=aqi_metric_list,
-                     description='Air Quality Metric'),
-                 renamed_ses_data=widgets.fixed(renamed_ses_data))
+    aqi_widget = widgets.interactive(
+        aqi_plotting,
+        ses_metric=widgets.Dropdown(
+            options=ses_metric_list,
+            description="Socioeconomic metric"),
+        aqi_metric=widgets.Dropdown(
+            options=aqi_metric_list,
+            description="Air Quality Metric"),
+        ses_data=widgets.fixed(ses_data))
 
-    return sensor, aqi
+    return sensor_widget, aqi_widget
 
-def display_app(renamed_ses_data):
+
+def display_app(ses_data):
     """Creates the layout for and renders the interactive app.
 
-    This function creates three output boxes for the three widgets and formats
-    them such that the map is on the left and the two scatter plots are stacked
-    vertically on the right.
-    Args:
-        - renamed_ses_data (GeoDataFrame): contains the census tract shapefile data
-        along with additional aqi computations. Some column names were renamed
- """
+    This function creates three output boxes for the three app widgets.
+    The map widget is on the left, and the two scatterplot widgets are
+    stacked vertically on the right.
 
-    # create the boxes to hold the widgets and format them
+    Args:
+        - ses_data (GeoDataFrame): contains census tract shapefile data
+          along with additional Purple Air sensor information.
+    """
+
+    # Create and format boxes to hold widgets.
     child1 = [widgets.Output()]
-    child23= [widgets.Output() for _ in range(2)]
-    right = widgets.VBox([child23[0],child23[1]])
+    child23 = [widgets.Output() for _ in range(2)]
+    right = widgets.VBox([child23[0], child23[1]])
     left = widgets.VBox(child1)
-    box = widgets.HBox([left,right])
-    box.layout.flex_flow='row'
-    box.layout.justify_content='center'
+    box = widgets.HBox([left, right])
+    box.layout.flex_flow = "row"
+    box.layout.justify_content = "center"
     display(box)
 
-    # create the scatter plot widgets
-    (sensor,aqi) = make_widgets(renamed_ses_data)
+    # Create the scatter plot widgets.
+    (sensor_widget, aqi_widget) = make_widgets(ses_data)
 
-    # display the widgets in their own boxes
+    # Display the widgets in their respective boxes.
     with child1[0]:
-        # widgets.interactive does not work with Altair charts, but interact does
-        widgets.interact(make_altair_chart,
-            renamed_ses_data=widgets.fixed(renamed_ses_data),
-            ses_metric = sorted(renamed_ses_data.keys()))
+        # Altair chart requires widgets.interact (not widgets.interactive).
+        widgets.interact(
+            make_altair_chart,
+            ses_data=widgets.fixed(ses_data),
+            ses_metric=sorted(ses_data.keys()))
 
     with child23[0]:
-        display(sensor)
+        display(sensor_widget)
 
     with child23[1]:
-        display(aqi)
+        display(aqi_widget)
+
+    return
